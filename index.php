f1c10c31760bfbe21a5c9632bd0dd9e2c4962278
CVITH\vithanage <kvithanage25@gmail.com>
2022-12-24 12:36:27 +0530

    Add features

--- index.php
+++ index.php
@@ -14,43 +14,57 @@
 <body>
     <!--head-->
     <div class="header">
-        <img src="waterDropLogo.png" height="90px" width="640px" alt="Logo" class="center">
+        <h1>WATER SUPPLY IMPEMENTATION SYSTEM</h1>
     </div>
 
     <div class="content">
-
-        <div class="column">
-            <div class="square">
-
+        <div class="column_left">
+            <div class="frame">
+                <div class="image"></div>
             </div>
         </div>
-
-        <div class="column">
+        <div class="column_right">
             <div class="square">
                 <br>
-                <h2 style="text-align: center; color: #187efa;">Log in</h2>
+                <h2 style="text-align: center; color: rgba(0, 42, 180, 0.675);">Log in</h2>
                 <form action="login.php" method="post">
 
+                    <!--
                     <?php if (isset($_GET['error'])) { ?>
                     <p class="error">
                         <?php echo $_GET['error']; ?>
                     </p>
-                    <?php } ?>
+                    <?php } ?>-->
 
-                    <label for="uname">Username</label>
-                    <input type="text" id="uname" name="uname" placeholder="Ex:- alex123..">
-                    <br>
+                    <div class="input-wrap">
+                        <input type="text" id="uname" name="uname" class="input-field">
+                        <label for="uname">Username</label>
+                    </div>
 
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password">
-                    <br>
+                    <div class="input-wrap">
+                        <input type="password" id="password" name="password" class="input-field">
+                        <label for="password">Password</label>
+                    </div>
 
-                    <button type="submit">Login</button>
+                    <div class="input-wrap">
+                        <select name="person" id="person" class="input-field" style="font-weight: bold; color:#6d6d6d;">
+                            <option value="">--Select type from here--</option>
+                            <option value="">Information Provider</option>
+                            <option value="">Donor</option>
+                            <option value="">Construction Company</option>
+                            <option value="">Admin</option>
+                        </select>
+                    </div>
+
+                    <button type="submit" class="login-btn">Login</button>
                 </form>
             </div>
         </div>
-
     </div>
+
+    <!--JavaScript file-->
+    <script src="login.js"></script>
+    
 </body>
 
 </html>
